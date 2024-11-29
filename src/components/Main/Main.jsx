9c04374676df05e0b7ac0f61bfc16d1b4c8b17b9
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-11-29 14:38:02 +0530

    finish the basic layout

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -16,26 +16,17 @@ const Main = () => {
                     <p>How can I help you today?</p>
                 </div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>
-                        Brainstorm team bonding activities for our work retreat
-                    </p>
-                    <img src={assets.bulb_icon} alt="" />
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder="Enter a prompt here" />
+                    <div>
+                        <img src={assets.send_icon} alt="" />
+                    </div>
                 </div>
-                <div className="card">
-                    <p>Suggest beautiful places to visit in India.</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Briefly summarize this concept: urban planning</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-
-                <div className="card">
-                    <p>Tell me about React js and React native</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
+                <p className="bottom-info">
+                    Fusion AI may display inaccurate info, including about
+                    people, so double-check its responses.
+                </p>
             </div>
         </div>
     );
